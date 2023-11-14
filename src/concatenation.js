ecb393c32be09f86ec93cf7ce30e0829e372ef32
wosean <serhii_v@ukr.net>
2023-11-15 00:20:38 +0200

    fix

--- src/concatenation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.concatenation = void 0;
-function concatenation(firstWord, secondWord) {
-    console.log("".concat(firstWord, " ").concat(secondWord));
-}
-exports.concatenation = concatenation;
